ff887fa29383b63b9e76676b1abf6d3ca14e7d5e
Grimo0 <gregoir.andre@gmail.com>
2021-07-05 10:02:26 +0200

    Change bowl appearance

--- src/Ingredient.hx
+++ src/Ingredient.hx
@@ -4,7 +4,7 @@ class Ingredient extends Interactive {
 	var game(get, never) : Game; inline function get_game() return Game.ME;
 
 	var id : String;
-	var content : h2d.Bitmap;
+	var content : HSprite;
 
 	public function new(id : String) {
 		super(0, 0);
@@ -12,17 +12,17 @@ class Ingredient extends Interactive {
 		this.id = id;
 		
 		var bowlBack = Assets.game.getBitmap('BowlBack', this);
-		content = Assets.game.getBitmap('Ingredient${id}', this);
+		content = new HSprite(Assets.game, 'Ingredient${id}', this);
 		var bowlFront = Assets.game.getBitmap('BowlFront', this);
 		
-		bowlBack.y = (content.tile.height/* - bowlBack.tile.height*/) / 2;
+		bowlBack.y = content.tile.height / 2;
 		bowlFront.y = bowlBack.y;
 
 		var size = getSize();
 		width = size.width;
 		height = size.height;
 
-		filter = new h2d.filter.DropShadow(0, 0.785, 0xCCCCFF, 1., 4, 1, 1, true);
+		filter = new h2d.filter.DropShadow(0, 0.785, 0x333333, 1., 4, 1, 1, true);
 		filter.enable = false;
 	}
 
