1fa36688b68c7211388f551f6f024b658cb9fd2b
Grimo0 <gregoir.andre@gmail.com>
2021-07-05 13:00:29 +0200

    Add sfx files

--- src/Assets.hx
+++ src/Assets.hx
@@ -2,7 +2,7 @@ import hxd.res.Sound;
 import dn.heaps.slib.*;
 
 class Assets {
-	// public static var SLIB = dn.heaps.assets.SfxDirectory.load("sfx");
+	public static var SLIB = dn.heaps.assets.SfxDirectory.load("sfx");
 
 	public static var fontPixel : h2d.Font;
 	public static var fontTiny : h2d.Font;
